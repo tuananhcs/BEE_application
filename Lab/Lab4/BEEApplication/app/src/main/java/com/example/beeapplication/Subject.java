9b3241fc79b57d14eb03fabad8aecdac81b90f2a
ntxmai5220 <xuanmai.k18@gmail.com>
2021-03-27 16:55:12 +0700

    "Update basic spinner"

--- Lab/Lab4/BEEApplication/app/src/main/java/com/example/beeapplication/Subject.java
+++ Lab/Lab4/BEEApplication/app/src/main/java/com/example/beeapplication/Subject.java
@@ -3,12 +3,26 @@ package com.example.beeapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Subject extends AppCompatActivity {
-
+    Spinner spinner;
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject);
+        addControl();
+    }
+
+    private void addControl() {
+        spinner=findViewById(R.id.spinner);
+        adapter=new ArrayAdapter<String>(Subject.this,
+                android.R.layout.simple_spinner_item);
+        adapter.add("  Học kì 2 2020-2021");
+        adapter.add("  Học kì 1 2020-2021");
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
