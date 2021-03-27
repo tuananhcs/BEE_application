df84cdaf02032b4287410367fa32f7bdcdc4b7fa
Dung <qd24052000@gmail.com>
2021-03-28 03:35:04 +0700

    thoikhoabieu

--- Lab/Lab4/BEEApplication/app/src/main/java/com/example/beeapplication/Timetable.java
+++ Lab/Lab4/BEEApplication/app/src/main/java/com/example/beeapplication/Timetable.java
@@ -3,6 +3,7 @@ package com.example.beeapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class Timetable extends AppCompatActivity {
@@ -15,6 +16,12 @@ public class Timetable extends AppCompatActivity {
         setContentView(R.layout.activity_timetable);
         addControls();
         addEvents();
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(Timetable.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
     }
 
     private void addEvents() {
